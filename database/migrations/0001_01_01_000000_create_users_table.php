37e6401f6512a9dc008c3039e198ed9daf494fc3
melspence <melvillespence@hamnavoesolutions.com>
2024-05-16 12:30:51 +0100

    Added Breeze

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('title', 30);
-            $table->string('title_of_courtesy',25)->nullable();
-
             $table->rememberToken();
             $table->timestamps();
         });
