133a221e3fbd879eeb1d352ac10ba2699b240e66
melspence <melvillespence@hamnavoesolutions.com>
2024-05-16 12:19:16 +0100

    Added SASS

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('title', 30);
+            $table->string('title_of_courtesy',25)->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
